624459211c9dde2f2672b43e4e6e9f04b161cf21
Dominick Baier <dbaier@leastprivilege.com>
2015-12-28 10:00:04 +0100

    removed code and added package dependencies

--- src/IdentityServer4.AccessTokenValidation/ScopeValidation/ScopeValidationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace IdentityServer4.AccessTokenValidation
-{
-    public class ScopeValidationOptions
-    {
-        public IEnumerable<string> AllowedScopes { get; set; }
-    }
-}
