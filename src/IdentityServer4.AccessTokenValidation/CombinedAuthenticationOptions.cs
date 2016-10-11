f8758a77f9c5adeb94ae2dd5a67a62d160def46e
Dominick Baier <dbaier@leastprivilege.com>
2016-10-11 16:28:43 +0200

    moved combined options creation to make it easier to modify underlying Jwt options

--- src/IdentityServer4.AccessTokenValidation/CombinedAuthenticationOptions.cs
+++ src/IdentityServer4.AccessTokenValidation/CombinedAuthenticationOptions.cs
@@ -2,18 +2,134 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using System;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace IdentityServer4.AccessTokenValidation
 {
     public class CombinedAuthenticationOptions
     {
+        static Func<HttpRequest, string> _tokenRetriever = request => request.HttpContext.Items["idsrv4:tokenvalidation:token"] as string;
+
         public string AuthenticationScheme { get; set; }
         public Func<HttpRequest, string> TokenRetriever { get; set; }
 
         public OAuth2IntrospectionOptions IntrospectionOptions { get; set; }
         public JwtBearerOptions JwtBearerOptions { get; set; }
+
+        public static CombinedAuthenticationOptions FromIdentityServerAuthenticationOptions(IdentityServerAuthenticationOptions options)
+        {
+            var combinedOptions = new CombinedAuthenticationOptions();
+            combinedOptions.TokenRetriever = options.TokenRetriever;
+            combinedOptions.AuthenticationScheme = options.AuthenticationScheme;
+
+            switch (options.SupportedTokens)
+            {
+                case SupportedTokens.Jwt:
+                    combinedOptions.JwtBearerOptions = ConfigureJwt(options);
+                    break;
+                case SupportedTokens.Reference:
+                    combinedOptions.IntrospectionOptions = ConfigureIntrospection(options);
+                    break;
+                case SupportedTokens.Both:
+                    combinedOptions.JwtBearerOptions = ConfigureJwt(options);
+                    combinedOptions.IntrospectionOptions = ConfigureIntrospection(options);
+                    break;
+                default:
+                    throw new Exception("SupportedTokens has invalid value");
+            }
+
+            return combinedOptions;
+        }
+
+        private static OAuth2IntrospectionOptions ConfigureIntrospection(IdentityServerAuthenticationOptions options)
+        {
+            var introspectionOptions = new OAuth2IntrospectionOptions
+            {
+                AuthenticationScheme = options.AuthenticationScheme,
+                Authority = options.Authority,
+                ScopeName = options.ScopeName,
+                ScopeSecret = options.ScopeSecret,
+
+                AutomaticAuthenticate = options.AutomaticAuthenticate,
+                AutomaticChallenge = options.AutomaticChallenge,
+
+                NameClaimType = options.NameClaimType,
+                RoleClaimType = options.RoleClaimType,
+
+                TokenRetriever = _tokenRetriever,
+                SaveToken = options.SaveToken,
+
+                EnableCaching = options.EnableCaching,
+                CacheDuration = options.CacheDuration,
+
+                DiscoveryTimeout = options.BackChannelTimeouts,
+                IntrospectionTimeout = options.BackChannelTimeouts
+            };
+
+            if (options.IntrospectionBackChannelHandler != null)
+            {
+                introspectionOptions.IntrospectionHttpHandler = options.IntrospectionBackChannelHandler;
+            }
+            if (options.IntrospectionDiscoveryHandler != null)
+            {
+                introspectionOptions.DiscoveryHttpHandler = options.IntrospectionDiscoveryHandler;
+            }
+
+            return introspectionOptions;
+        }
+
+        private static JwtBearerOptions ConfigureJwt(IdentityServerAuthenticationOptions options)
+        {
+            var jwtOptions = new JwtBearerOptions
+            {
+                AuthenticationScheme = options.AuthenticationScheme,
+                Authority = options.Authority,
+                RequireHttpsMetadata = options.RequireHttpsMetadata,
+
+                AutomaticAuthenticate = options.AutomaticAuthenticate,
+                AutomaticChallenge = options.AutomaticChallenge,
+
+                BackchannelTimeout = options.BackChannelTimeouts,
+                RefreshOnIssuerKeyNotFound = true,
+
+                SaveToken = options.SaveToken,
+
+                Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = e =>
+                    {
+                        e.Token = _tokenRetriever(e.Request);
+                        return options.JwtBearerEvents.MessageReceived(e);
+                    },
+                    OnTokenValidated = e => options.JwtBearerEvents.TokenValidated(e),
+                    OnAuthenticationFailed = e => options.JwtBearerEvents.AuthenticationFailed(e),
+                    OnChallenge = e => options.JwtBearerEvents.Challenge(e)
+                }
+            };
+
+            if (options.JwtBackChannelHandler != null)
+            {
+                jwtOptions.BackchannelHttpHandler = options.JwtBackChannelHandler;
+            }
+
+            jwtOptions.TokenValidationParameters.ValidateAudience = false;
+            jwtOptions.TokenValidationParameters.NameClaimType = options.NameClaimType;
+            jwtOptions.TokenValidationParameters.RoleClaimType = options.RoleClaimType;
+
+            if (options.InboundJwtClaimTypeMap != null)
+            {
+                var handler = new JwtSecurityTokenHandler();
+                handler.InboundClaimTypeMap = options.InboundJwtClaimTypeMap;
+
+                jwtOptions.SecurityTokenValidators.Clear();
+                jwtOptions.SecurityTokenValidators.Add(handler);
+            }
+
+            return jwtOptions;
+        }
     }
 }
--- src/IdentityServer4.AccessTokenValidation/IdentityServerAuthenticationExtensions.cs
+++ src/IdentityServer4.AccessTokenValidation/IdentityServerAuthenticationExtensions.cs
@@ -3,44 +3,28 @@
 
 
 using IdentityServer4.AccessTokenValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Http;
-using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Builder
 {
     public static class IdentityServerAuthenticationExtensions
     {
-        static Func<HttpRequest, string> _tokenRetriever = request => request.HttpContext.Items["idsrv4:tokenvalidation:token"] as string;
-
         public static IApplicationBuilder UseIdentityServerAuthentication(this IApplicationBuilder app, IdentityServerAuthenticationOptions options)
         {
-            var combinedOptions = new CombinedAuthenticationOptions();
-            combinedOptions.TokenRetriever = options.TokenRetriever;
-            combinedOptions.AuthenticationScheme = options.AuthenticationScheme;
-            
-            switch (options.SupportedTokens)
-            {
-                case SupportedTokens.Jwt:
-                    combinedOptions.JwtBearerOptions = ConfigureJwt(options);
-                    break;
-                case SupportedTokens.Reference:
-                    combinedOptions.IntrospectionOptions = ConfigureIntrospection(options);
-                    break;
-                case SupportedTokens.Both:
-                    combinedOptions.JwtBearerOptions = ConfigureJwt(options);
-                    combinedOptions.IntrospectionOptions = ConfigureIntrospection(options);
-                    break;
-                default:
-                    throw new Exception("SupportedTokens has invalid value");
-            }
+            var combinedOptions = CombinedAuthenticationOptions.FromIdentityServerAuthenticationOptions(options);
+            app.UseIdentityServerAuthentication(combinedOptions);
 
-            app.UseMiddleware<IdentityServerAuthenticationMiddleware>(app, combinedOptions);
+            return app.UseScopeValidation(options);
+        }
 
+        public static IApplicationBuilder UseIdentityServerAuthentication(this IApplicationBuilder app, CombinedAuthenticationOptions options)
+        {
+            return app.UseMiddleware<IdentityServerAuthenticationMiddleware>(options);    
+        }
+
+        public static IApplicationBuilder UseScopeValidation(this IApplicationBuilder app, IdentityServerAuthenticationOptions options)
+        {
             if (options.ValidateScope)
             {
                 var allowedScopes = new List<string>();
@@ -67,93 +51,6 @@ namespace Microsoft.AspNetCore.Builder
             }
 
             return app;
-        }
-
-        private static OAuth2IntrospectionOptions ConfigureIntrospection(IdentityServerAuthenticationOptions options)
-        {
-            var introspectionOptions = new OAuth2IntrospectionOptions
-            {
-                AuthenticationScheme = options.AuthenticationScheme,
-                Authority = options.Authority,
-                ScopeName = options.ScopeName,
-                ScopeSecret = options.ScopeSecret,
-
-                AutomaticAuthenticate = options.AutomaticAuthenticate,
-                AutomaticChallenge = options.AutomaticChallenge,
-
-                NameClaimType = options.NameClaimType,
-                RoleClaimType = options.RoleClaimType,
-
-                TokenRetriever = _tokenRetriever,
-                SaveToken = options.SaveToken,
-
-                EnableCaching = options.EnableCaching,
-                CacheDuration = options.CacheDuration,
-
-                DiscoveryTimeout = options.BackChannelTimeouts,
-                IntrospectionTimeout = options.BackChannelTimeouts
-            };
-
-            if (options.IntrospectionBackChannelHandler != null)
-            {
-                introspectionOptions.IntrospectionHttpHandler = options.IntrospectionBackChannelHandler;
-            }
-            if (options.IntrospectionDiscoveryHandler != null)
-            {
-                introspectionOptions.DiscoveryHttpHandler = options.IntrospectionDiscoveryHandler;
-            }
-
-            return introspectionOptions;
-        }
-
-        private static JwtBearerOptions ConfigureJwt(IdentityServerAuthenticationOptions options)
-        {
-            var jwtOptions = new JwtBearerOptions
-            {
-                AuthenticationScheme = options.AuthenticationScheme,
-                Authority = options.Authority,
-                RequireHttpsMetadata = options.RequireHttpsMetadata,
-
-                AutomaticAuthenticate = options.AutomaticAuthenticate,
-                AutomaticChallenge = options.AutomaticChallenge,
-
-                BackchannelTimeout = options.BackChannelTimeouts,
-                RefreshOnIssuerKeyNotFound = true,
-
-                SaveToken = options.SaveToken,
-
-                Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = e =>
-                    {
-                        e.Token = _tokenRetriever(e.Request);
-                        return options.JwtBearerEvents.MessageReceived(e);
-                    },
-                    OnTokenValidated = e => options.JwtBearerEvents.TokenValidated(e),
-                    OnAuthenticationFailed = e => options.JwtBearerEvents.AuthenticationFailed(e),
-                    OnChallenge = e => options.JwtBearerEvents.Challenge(e)
-                }
-            };
-
-            if (options.JwtBackChannelHandler != null)
-            {
-                jwtOptions.BackchannelHttpHandler = options.JwtBackChannelHandler;
-            }
-
-            jwtOptions.TokenValidationParameters.ValidateAudience = false;
-            jwtOptions.TokenValidationParameters.NameClaimType = options.NameClaimType;
-            jwtOptions.TokenValidationParameters.RoleClaimType = options.RoleClaimType;
-
-            if (options.InboundJwtClaimTypeMap != null)
-            {
-                var handler = new JwtSecurityTokenHandler();
-                handler.InboundClaimTypeMap = options.InboundJwtClaimTypeMap;
-
-                jwtOptions.SecurityTokenValidators.Clear();
-                jwtOptions.SecurityTokenValidators.Add(handler);
-            }
-            
-            return jwtOptions;
         }
     }
 }
