5497fdc2fe556ed104514bbfcd46e5011495d399
Dominick Baier <dbaier@leastprivilege.com>
2019-10-22 09:32:28 +0200

    update packages

--- src/IdentityServerAuthenticationDefaults.cs
+++ src/IdentityServerAuthenticationDefaults.cs
@@ -13,6 +13,11 @@ namespace IdentityServer4.AccessTokenValidation
         /// </summary>
         public const string AuthenticationScheme = "Bearer";
 
+        /// <summary>
+        /// Value of the JWT typ header (IdentityServer4 v3+ sets this by default)
+        /// </summary>
+        public const string JwtAccessTokenTyp = "at+jwt";
+
         internal const string IntrospectionAuthenticationScheme = "IdentityServerAuthenticationIntrospection";
         internal const string JwtAuthenticationScheme = "IdentityServerAuthenticationJwt";
         internal const string TokenItemsKey = "idsrv4:tokenvalidation:token";
